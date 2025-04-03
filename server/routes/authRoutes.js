0a3304d33af369973e51796a4684a0ee681cf34f
Aa-sheesh <aashish4912345@gmail.com>
2025-04-03 21:57:14 +0530

    remove unused database configuration and routes; update package.json and server.js for cleaner structure

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,20 @@
 import express from "express";
 
 const router = express.Router();
+router.get("/", (req, res) => {
+  res.send('Hello from "api/v1"');
+});
 
 router.get("/login",(req,res)=>{
-    res.send("Hello from login route")
-});
-router.get("/signup",(req,res)=>{
-    res.send("Hello from signup route")
-});
-router.get("/logout",(req,res)=>{
-    res.send("Hello from logout route")
+    res.send('Login route');
 });
 
+router.get("/logout",(req,res)=>{
+    res.send('Logout route');
+});
+
+router.get("/signup",(req,res)=>{
+    res.send('signup route');
+});
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,20 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
-import authRoutes from "./routes/authRoutes.js"; 
-import resumeRoutes from "./routes/resumeRoutes.js"; 
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
-const app = express();
+
+const app=express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.send(`Hello from "/" `);
-});
+app.listen(process.env.PORT||3000,(req,res)=>{
+    console.log(`Server is running on port ${process.env.PORT||3000}`);
+    console.log(`http://localhost:${process.env.PORT||3000}`);
+})
 
 app.use("/api/v1",authRoutes);
-app.use("/api/v1",resumeRoutes );
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
